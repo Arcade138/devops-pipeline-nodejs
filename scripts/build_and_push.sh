f8cca43ec0bc963df55b8b2f11b06d286d4925ea
Arcade138 <arc2024diya@gmail.com>
2025-07-28 15:54:41 +0530

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -6,7 +6,7 @@ docker build -t diyacapg/myapp:$(git rev-parse --short HEAD) .
 docker tag diyacapg/myapp:$(git rev-parse --short HEAD) diyacapg/myapp:latest
 
 echo "[INFO] Logging in to DockerHub"
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+echo "seS*68SeiRYSL*p" | docker login -u "diyacapg" --password-stdin
 
 echo "[INFO] Pushing Docker image with tag $(git rev-parse --short HEAD)"
 docker push diyacapg/myapp:$(git rev-parse --short HEAD)
